4981128987311a346800c297c82a62707bd70947
nijh <1059186249@qq.com>
2026-01-06 13:18:59 +0800

    feat:修改了user类

--- demo1/src/main/java/com/example/domain/User.java
+++ demo1/src/main/java/com/example/domain/User.java
@@ -1,9 +1,11 @@
 package com.example.domain;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 @TableName("user")
 public class User {
     private Integer user_id;
@@ -11,45 +13,4 @@ public class User {
     private String password;
     private LocalDateTime create_at;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "user_id=" + user_id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", create_at='" + create_at + '\'' +
-                '}';
-    }
-
-    public Integer getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public LocalDateTime getCreate_at() {
-        return create_at;
-    }
-
-    public void setCreate_at(LocalDateTime create_at) {
-        this.create_at = create_at;
-    }
 }
